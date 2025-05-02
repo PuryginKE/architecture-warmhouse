7f32d1e7523febffd2e4986d8f14217ab0e90383
Kirill <eddy-95@mail.ru>
2025-05-02 17:42:07 +0300

    add task 5

--- apps/smart_home/handlers/sensors.go
+++ apps/smart_home/handlers/sensors.go
@@ -54,7 +54,6 @@ func (h *SensorHandler) GetSensors(c *gin.Context) {
 	for i, sensor := range sensors {
 		if sensor.Type == models.Temperature {
 			tempData, err := h.TemperatureService.GetTemperatureByID(fmt.Sprintf("%d", sensor.ID))
-			log.Println("TEST3", sensor.ID)
 			if err == nil {
 				// Update sensor with real-time data
 				sensors[i].Value = tempData.Value
